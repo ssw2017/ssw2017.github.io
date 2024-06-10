538cd952eac40e6c7140a71272b750d4131a2ad5
Shanti Gundam <shantiswarupa@yahoo.com>
2024-06-10 15:58:29 -0500

    uploading fix for sound
    uploading fix for sound

--- whack-a-mole/js-dev-final-capstone-starter-whack-a-mole/src/index.js
+++ whack-a-mole/js-dev-final-capstone-starter-whack-a-mole/src/index.js
@@ -1,10 +1,10 @@
-cconst holes = document.querySelectorAll('.hole');
+const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
 const score = document.querySelector('#score'); // Use querySelector() to get the score element
 const timerDisplay = document.querySelector('#timer'); // use querySelector() to get the timer element.
 
-const hammerSound = new Audio("https://github.com/ssw2017/ssw2017.github.io/blob/main/whack-a-mole/js-dev-final-capstone-starter-whack-a-mole/assets/bubleForHit.mp3?raw=true");
+const hammerSound = new Audio("https://github.com/ssw2017/js-dev-final-capstone-starter-whack-a-mole/blob/main/assets/bubleForHit.mp3?raw=true");
 const hammerCursor = document.querySelector('#hammer');
 
 let time = 0;
